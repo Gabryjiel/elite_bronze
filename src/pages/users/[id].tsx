fbf4c0a1a9d554349924953efaba848dea499031
Gabryjiel <gabriel.kudyba@gmail.com>
2023-01-25 18:13:31 +0100

    cleanup

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import { useRouter } from "next/router";
 
 import { ContentContainer } from "../../components/common/ContentContainer";
